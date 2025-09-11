e1c2af217148a20cbb8f06fd025086772b952c13
seodowa <pagalan.theodoreustp@gmail.com>
2025-09-11 17:28:58 +0800

    Add basic project structure

---
+++ components/NavigationBar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NavigationBar() {
+    const navigation = ["Product", "Features", "Pricing", "Company", "Blog"];
+
+    return (
+        <div className="w-full bg-white shadow-md sticky top-0">
+            test
+        </div>
+    );
+}
