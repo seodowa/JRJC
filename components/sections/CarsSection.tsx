84770e146c1623782cb2d8c4dcce5876634fdcc4
seodowa <pagalan.theodoreustp@gmail.com>
2025-09-24 17:05:41 +0800

    Add cars available placeholder

--- components/sections/CarsSection.tsx
+++ components/sections/CarsSection.tsx
@@ -2,8 +2,10 @@ import "@/app/globals.css";
 
 export default function CarsSection() {
   return (
-    <section id="cars" className="min-h-screen">
-        <h1>Test</h1>
+    <section id="cars" className="min-h-screen bg-main-color flex flex-col items-center pt-16">
+        <h1 className="font-semibold text-7xl py-16">Cars Available</h1>
+        <div className="border-2 w-screen h-85">
+        </div>
     </section>
   );
 }
