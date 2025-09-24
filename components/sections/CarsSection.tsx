1844748b0ebad7cf4fd11f7c11bc7094f6e43419
seodowa <pagalan.theodoreustp@gmail.com>
2025-09-24 16:10:55 +0800

    Fix minor details in hero section

--- components/sections/CarsSection.tsx
+++ components/sections/CarsSection.tsx
@@ -2,7 +2,7 @@ import "@/app/globals.css";
 
 export default function CarsSection() {
   return (
-    <section id="cars">
+    <section id="cars" className="min-h-screen">
         <h1>Test</h1>
     </section>
   );
--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -2,11 +2,11 @@ import "@/app/globals.css";
 
 export default function HeroSection() {
   return (
-    <section id="hero" className="bg-gradient-to-b from-main-color to-secondary-50 min-h-screen
+    <section id="hero" className="bg-main-color min-h-screen
                     flex flex-col items-center -mt-12
                     md:flex-row-reverse">
-       <div className="min-h-screen flex flex-col justify-start items-center w-full
-                       md:h-full md:w-1/2 md:justify-start pt-46 md:pl-20 
+       <div className="flex flex-col items-center w-full h-1/2
+                       md:min-h-screen md:w-1/2 pt-40 md:pt-60 md:pl-20 
                        md:bg-gradient-to-l from-main-color from-85% to-transparent z-1">
           <div className="flex flex-col justify-center items-center">
             <h1 className="font-extrabold text-8xl text-[#578FCA]">JRJC</h1>
@@ -16,8 +16,8 @@ export default function HeroSection() {
                                   shadow-md">Book Now</a>
           </div>
        </div>
-       <div className="absolute left-0 top-1/2 md:top-auto">
-          <img src="/images/kentb_car_gradient.webp" className="opacity-30 min-w-5xl "/>
+       <div className="flex overflow-hidden object-fit absolute left-0 top-4/11 md:top-auto w-full">
+          <img src="/images/kentb_car_gradient.webp" className="opacity-30 min-w-5xl"/>
        </div>
     </section>
   );
