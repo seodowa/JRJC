0786b1ff8837b25bef1d73c95160001e07aea38f
seodowa <pagalan.theodoreustp@gmail.com>
2025-11-23 22:37:50 +0800

    Modify cars section in landing page to not display cars in maintenance

--- components/sections/CarsSection.tsx
+++ components/sections/CarsSection.tsx
@@ -6,12 +6,17 @@ import { fetchCars } from "@/lib/supabase/queries/fetchCars";
 export default async function CarsSection() {
   const CAROUSEL_HEIGHT = 38 * 16; // rem * 16 = px
   const cars = await fetchCars();
+  const IN_MAINTENANCE = 3; // status id for car in maintenance
   
   return (
     <section id="cars" className="min-h-screen relative bg-gradient-to-b from-main-color to-secondary-50 flex flex-col items-center pt-16">
         <h1 className="font-main-font text-4xl sm:text-5xl md:text-6xl md:py-10 text-center">Cars Available</h1>
         <Carousel height={CAROUSEL_HEIGHT}>
-          {cars.map(car => <CarCard key={car.id} car={car} />)}
+          {cars.map(car => {
+              if (car.status?.id === IN_MAINTENANCE) return
+
+              return <CarCard key={car.id} car={car} />
+            })}
         </Carousel>
     </section>
   );
