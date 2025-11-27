7d44b4e7dce1ae41ac2bccf09f47fb3ee6206a21
seodowa <pagalan.theodoreustp@gmail.com>
2025-11-27 22:47:27 +0800

    Add logo to homepage

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -6,10 +6,11 @@ export default function HeroSection() {
                     flex flex-col items-center -mt-12 overflow-hidden
                     md:flex-row-reverse">
        <div className="flex flex-col items-center w-full h-1/2
-                       md:min-h-screen md:w-3/5 pt-30 md:pt-60 md:pl-20 
+                       md:min-h-screen md:w-3/5 pt-40 md:pt-50 md:pl-20 
                        md:bg-gradient-to-l from-main-color from-85% to-transparent z-1">
           <div className="flex flex-col justify-center items-center">
-            <h1 className="font-extrabold text-8xl text-[#578FCA]">JRJC</h1>
+            <img src="/images/JRJC TEXT ONLY.png" alt="" className="min-h-40 max-h-40 md:min-h-50 md:max-h-50"/>
+            {/* <h1 className="font-extrabold text-8xl text-[#578FCA]">JRJC</h1> */}
             <h3 className="pt-2 pb-9 text-2xl">Car Rental Services</h3>
             <a href="book" className="bg-secondary-100 py-2 md:py-3 px-16 md:px-16 
                                   text-2xl md:text-3xl text-white rounded-full transition-transform hover:scale-110
